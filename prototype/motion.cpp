ef39025c3d648b947b7b7f8c250eec756d1d9125
James Smith <codefiend@gmail.com>
2014-11-15 15:17:31 -0800

    Initial prototype commit

---
+++ prototype/motion.cpp
@@ -0,0 +1,15 @@
+/*
+ * Sierra College Computer Engineering Club: Sparkfun AVC
+ * Motion Subsystem
+ * Authors: 
+ */
+#include "motion.h"
+
+void initMotion()
+{
+}
+
+void updateMotion()
+{
+}
+
---
+++ prototype/nav.h
@@ -0,0 +1,14 @@
+/*
+ * Sierra College Computer Engineering Club: Sparkfun AVC
+ * Navigation Subsystem
+ * Authors: 
+ */
+
+#ifndef _NAV_H_
+#define _NAV_H_
+
+void initNav();
+void updateNav();
+
+#endif
+
