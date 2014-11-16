b5f3e274ebef1e54c733216652ccd6a2862c58da
James Smith <codefiend@gmail.com>
2014-11-15 16:24:44 -0800

    Adding scheduler
    Tasks are now scheduled

---
+++ src/inc/schedule.h
@@ -0,0 +1,28 @@
+/*
+ * schedule.h
+ *
+ *  Created on: Nov 1, 2014
+ *      Author: james
+ */
+
+#ifndef _SCHEDULE_H_
+#define _SCHEDULE_H_
+
+#define SCH_MAX_TASKS (3)
+
+typedef struct
+{
+	void (* taskFunction)(void);
+	uint16_t delay;
+	uint16_t period;
+	uint8_t runMe;
+} schTask;
+
+void schInit();
+uint8_t schAddTask(void (* function)(), const uint16_t DELAY, const uint16_t PERIOD);
+uint8_t schDeleteTask( uint8_t i );
+void schUpdate();
+void schDispatchTasks();
+void schStart();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -8,10 +8,11 @@
 #include <stdint.h>
 
 /* TI Drivers */
-/*#include "sys_common.h"*/
-/*#include "rti.h"*/
+#include "sys_common.h"
+#include "rti.h"
 
 /* AVC */
+#include "schedule.h"
 #include "sensors.h"
 #include "navigation.h"
 #include "motion.h"
@@ -19,14 +20,31 @@
 int counter = 0;
 int main( void )
 {
+	schInit();
+	rtiInit();
+	rtiEnableNotification(rtiNOTIFICATION_COMPARE0);
+	_enable_IRQ();
+	rtiStartCounter(rtiCOUNTER_BLOCK0);
+
 	sensorsInit();
 	navigationInit();
 	motionInit();
 
+	schAddTask( sensorsUpdate, 0, 10 );
+	schAddTask( navigationUpdate, 0, 30 );
+	schAddTask( motionUpdate, 0, 30 );
+
+	schStart();
 	while( 1 )
 	{
-		sensorsUpdate();
-		navigationUpdate();
-		motionUpdate();
+		/* TODO: the update call will move to a hardware timer eventually */
+		schUpdate();
+		schDispatchTasks();
 	}
+}
+
+void rtiNotification(uint32 notification)
+{
+	//printf("Timer\n");
+	counter++;
 }
