83615c72694f09ea312d91e014dc7cfaa659e333
Kristoffer Semelka <soderstroff@gmail.com>
2014-12-04 20:35:57 -0800

    Created rough outline of updateNav function.

--- prototype/nav.cpp
+++ prototype/nav.cpp
@@ -1,7 +1,7 @@
 /*
  * Sierra College Computer Engineering Club: Sparkfun AVC
  * Navigation Subsystem
- * Authors: 
+ * Authors: Kristoffer Semelka
  */
 #include "common.h"
 #include "nav.h"
@@ -11,10 +11,34 @@ motionData MotionData;
 
 void initNav()
 {
+  /*
+    Nothing goes here yet.
+    For future milestones, include Bezier curve data here.
+  */
 }
 
 void updateNav()
 {
+  /* This function:
+     - gets the navigation data from sensors;
+     - "gets" (constant for now) desired heading;
+     - finds difference;
+     - applies PID (just P for now);
+     - creates instructions for motion module;
+     - and pushes instructions to MotionData
+     on every update.
+  */
+  navData* NavData;
+  float desiredHeading;
+  float headingDiff;
+  
+  NavData = getNavData();
+  desiredHeading = M1HEADING;
+
+  headingDiff = findDifference(NavData->heading, M1HEADING);
+
+  MotionData.heading = headingDiff;
+  
 }
 
 motionData* getMotionData()
