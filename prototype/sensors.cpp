c68a0025e50587512f010b0c26a07506c2f5583f
Alex Norell <alex@alexnorell.com>
2014-12-05 02:01:53 -0800

    Basic magnetometer functions done
    The basic functions are written, now they need to be tested and fixed.

--- prototype/sensors.cpp
+++ prototype/sensors.cpp
@@ -5,10 +5,10 @@
  * 
  * Sensor: Hitachi HM55B
  * Board: Paralax HM55B REV B 29123
- *
+ *	
  *           ╒══════════════════╕
  *           │ ▀▀▀   ▄▄▄▄   ▀▀▀ │ 
- *           │▄███   ▀▀▀▀   ███▄│ 
+ *           │ ▄▄▄   ▀▀▀▀   ▄▄▄ │ 
  *    Din (1)│██▄█▌        ▐█▄██│(6) Vcc
  *           │████▌▐██████▌▐████│ 
  *   Dout (2)│██▄█▌▐██████▌▐█▄██│(5) /EN
@@ -28,18 +28,149 @@
  */
 #include "common.h"
 #include "sensors.h"
+#include <math.h>
+
+/* Debugging Flag */
+const int DEBUGGING = 1;
 
 navData NavData;
 
 /* Pins */
-const byte CLK = ;
-const byte EN = ;
-const byte DATA =;
+const byte CLK = 10;
+const byte EN = 11;
+const byte DATA = 12;
+
+/* HM55B Commands */
+int hm55b_shift_in( int bits )
+{
+	pinMode( DATA, INPUT );
+	int result = 0;
+	
+	if ( DEBUGGING )
+	{
+		Serial.print( "Shifting in ");
+		Serial.print( bits );
+		Serial.print( "Recieved: " );
+	}
+	
+	for( int i = bits; i >= 0; i-- )
+	{
+		digitalWrite( CLK, HIGH );
+		delayMicroseconds(1);
+		if ( digitalRead( DATA ) == HIGH )
+		{
+			result = ( result << 1 ) + 1;
+			if ( DEBUGGING )
+			{
+				Serial.print( "1" );
+			}
+		}
+		else
+		{
+			result = result << 1;
+			if ( DEBUGGING )
+			{
+				Serial.print( "0" );
+			}
+		}
+		digitalWrite( CLK, LOW );
+		delayMicroseconds(1);
+	}
+	if ( DEBUGGING )
+	{
+		Serial.print( "\n" );
+	}
+	
+	if ( ( result & 1 << 11 ) == 1 << 11 )
+	{
+		result = ( B11111000 << 8 ) | result;
+	}
+	return result;
+}
+
+void hm55b_shift_out ( int value, int bits )
+{
+	if ( DEBUGGING )
+	{
+		Serial.print( "Shifting out " );
+		Serial.print( bits );
+		Serial.print( " bits. Sending " ); 
+		Serial.print( value );
+		Serial.print(" Sent: " );
+	}
+	for( int i = bits; i >= 0; i-- )
+	{
+		digitalWrite( CLK, LOW );
+		if ( ( value & 1 << i ) == ( 1 << i ) )
+		{
+			digitalWrite( DATA, HIGH );
+			if ( DEBUGGING )
+			{
+				Serial.print( "1" );
+			}
+		}
+		else
+		{
+			digitalWrite( DATA, LOW );
+			if ( DEBUGGING )
+			{
+				Serial.print( "0" );
+			}
+		}
+		digitalWrite( CLK, HIGH );
+		delayMicroseconds(1);
+	}
+	if ( DEBUGGING )
+	{
+		Serial.print( "\n" );
+	}
+	
+}
+
+void hm55b_reset()
+{
+	pinMode( DATA, OUTPUT );
+	digitalWrite( EN, LOW );
+	hm55b_shift_out( B0000, 3 );
+	digitalWrite( EN, HIGH);
+	if ( DEBUGGING )
+	{
+		Serial.print( "Reset HM55B\n" );
+	}
+}
+
+int hm55b_read()
+{
+	pinMode( DATA, OUTPUT );
+	digitalWrite( EN, LOW );
+	hm55b_shift_out( B0000, 3 );
+	digitalWrite( EN, HIGH );
+	delay(40);
+	
+	pinMode( DATA, OUTPUT );
+	digitalWrite( EN, LOW );
+	hm55b_shift_out( B1100, 3 );
+	int read_command_result = hm55b_shift_in( 3 );
+	
+	int x_value = hm55b_shift_in( 11 );
+	int y_value = hm55b_shift_in( 11 );
+	
+	digitalWrite( EN, HIGH );
+	
+	int angle = 180 * ( atan2( ( -1 * y_value ) , x_value ) / M_PI );
+	if ( DEBUGGING )
+	{
+		Serial.print( "Angle: " );
+		Serial.print( angle );
+		Serial.print( "\n" );
+	}
+	return angle;
+}
+
 
 void initSensors()
 {
 	
-
 }
 
 void updateSensors()
