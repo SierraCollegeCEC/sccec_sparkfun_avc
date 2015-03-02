2ddffe7bf7ecdbb1b9613387098cf54345ec5ef7
Kristoffer Semelka <soderstroff@gmail.com>
2015-03-01 23:25:51 -0800

    -

---
+++ prototype/kalman.h
@@ -0,0 +1,20 @@
+/*
+ * Sierra College Computer Engineering Club: Sparkfun AVC
+ * Kalman Filter
+ * Authors: Kristoffer Semelka, Austin
+ */
+
+#ifndef _KALMAN_H_
+#define _KALMAN_H_
+
+/*
+
+To-do: rename interface in sensors.h to getSensorsData().
+Implement initfilter().
+  
+*/
+
+void getNavData();
+/* Initiates an entire time step of the Kalman filter. */
+
+#endif
