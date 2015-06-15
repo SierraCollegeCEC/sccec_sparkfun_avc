782c6cd0a8f7c459402194cfd4152064a0da9b0f
Kristoffer Semelka <ksemelka@gmail.com>
2015-06-15 16:23:34 -0700

    Including telemetry header also includes standard string lib.

--- src/inc/telemetry.h
+++ src/inc/telemetry.h
@@ -7,6 +7,8 @@
 #ifndef _TELEMETRY_H_
 #define _TELEMETRY_H_
 
+#include <string.h> /* Any file that adds a telemetry handler will need strcmp */
+
 /*
  * This is the typedef for function pointers that will respond to events from the telemetry
  * system. These events are fired when messages are recieved from the reciever
--- src/nav.c
+++ src/nav.c
@@ -10,7 +10,6 @@
 #include <math.h>
 #include "pid.h"
 #include "bezier.h"
-#include "string.h"
 #include "telemetry.h"
 
 motionData MotionData;
